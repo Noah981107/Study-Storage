829b0ebeded10214f11fd7b13c1d4f31fb554213
Noah <76483681+Noah981107@users.noreply.github.com>
2021-01-06 14:56:17 +0900

    Save modified Content
    Save modified Content

--- main.js
+++ main.js
@@ -127,6 +127,25 @@ var app = http.createServer(function(request,response){
         });
       });
     }
+    else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data',function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error){
+          fs.writeFile(`data/${title}`, description, 'utf8',
+          function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end('success');
+          });
+        });
+      });
+    }
     else {
       response.writeHead(404);
       response.end('Not found');
