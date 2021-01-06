50fd6aa4891f6da29c63d5813553b77121fa2db4
Noah <76483681+Noah981107@users.noreply.github.com>
2021-01-06 16:39:11 +0900

    Using Lib
    Using Lib

--- main.js
+++ main.js
@@ -3,37 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
 
-var template = {
-  HTML:function(title, list, body, control) {
-    return `
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <meta charset = "utf-8">
-        <title> 광주 풋살 매칭 시스템</title>
-        <link rel = "stylesheet" href = "style.css">
-      </head>
-      <body>
-        <h1> <a href = "/">광주 풋살 매칭 시스템</a> </h1>
-        <div id = "grid">
-          ${list}
-          ${control}
-          ${body}
-        </div>
-      </body>
-    </html>
-    `;
-  }, LIST :function(filelist) {
-    var list = `<ul>`;
-    var i = 0 ;
-    while(i<filelist.length) {
-      list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-      i = i + 1;
-    }
-    list = list + `</ul>`;
-    return list;
-  }
-}
+var template = require('./lib/template.js');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
